090bc1e0aad3c04333d430d938a14acf5f1cf6b4
nickleduc91 <122701320+nickleduc91@users.noreply.github.com>
2023-05-05 23:44:52 -0400

    Small fix

--- components/BestUses.tsx
+++ components/BestUses.tsx
@@ -35,7 +35,7 @@ const BestUses = () => {
   return (
     <div className="text-gray-400">
       <div className="pt-14 text-center">
-        <h3 className="text-3xl md:text-3xl xl:text-4xl font-bold tracking-tight mb-2">
+        <h3 className="text-white text-3xl md:text-3xl xl:text-4xl font-bold tracking-tight mb-2">
           Productive Uses for
           <span className="text-cyan-500"> TwoDue</span>
         </h3>
