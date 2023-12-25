5052fbcef09a70e68ee05fc72a2cd7d675dec9a9
nickleduc91 <122701320+nickleduc91@users.noreply.github.com>
2023-12-25 17:51:19 -0500

    Added meta data for /boards page

--- pages/boards/index.jsx
+++ pages/boards/index.jsx
@@ -3,10 +3,27 @@ import BoardsList from "../../components/boardsList";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "pages/api/auth/[...nextauth]";
 import Footer from "../../components/footer";
+import Head from 'next/head'
 
 const BoardsPage = ({ user, boards }) => {
   return (
     <div className="bg-gray-100 dark:bg-slate-900 min-h-screen">
+      <Head>
+        <title>Boards - TwoDue</title>
+        <meta property="og:title" content="Boards - TwoDue"></meta>
+        <meta
+          name="description"
+          content="Explore your personalized boards on TwoDue! Navigate your tasks seamlessly by accessing all your boards in one place. Stay organized and in control with TwoDue!"
+        />
+        <meta
+          property="og:description"
+          content="Explore your personalized boards on TwoDue! Navigate your tasks seamlessly by accessing all your boards in one place. Stay organized and in control with TwoDue!"
+        />
+        <meta
+          property="og:url"
+          content="https://twodue.vercel.app/boards"
+        ></meta>
+      </Head>
       <Header isSession={true} user={user} />
       <BoardsList userId={user._id} boards={boards} />
       <Footer />
@@ -28,7 +45,7 @@ export async function getServerSideProps(context) {
   //fetch user
   const user = await fetch(`${process.env.URL}/api/users/${session.user.id}`);
   let userData = await user.json();
-  
+
   //fetch for boards based off id
   const boards = await fetch(
     `${process.env.URL}/api/boards?userId=${userData._id}`
